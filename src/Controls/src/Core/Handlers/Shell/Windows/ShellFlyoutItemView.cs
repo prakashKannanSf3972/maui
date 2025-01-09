5ba5f23647dfc2b371c6e51b25ae81b5836f826b
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-01-09 16:27:44 +0530

    Fix-Shell-ItemView

--- src/Controls/src/Core/Handlers/Shell/Windows/ShellFlyoutItemView.cs
+++ src/Controls/src/Core/Handlers/Shell/Windows/ShellFlyoutItemView.cs
@@ -128,7 +128,7 @@ namespace Microsoft.Maui.Controls.Platform
 
 		protected override global::Windows.Foundation.Size ArrangeOverride(global::Windows.Foundation.Size finalSize)
 		{
-			if (this.ActualWidth > 0 && _content is IView view)
+			if (finalSize.Width > 0 && _content is IView view)
 			{
 				view.Arrange(new Rect(0, 0, finalSize.Width, finalSize.Height));
 				return finalSize;
