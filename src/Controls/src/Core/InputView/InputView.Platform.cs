04d897b3e99d437b029b095c1022641fac117850
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-01-21 18:38:36 +0530

    Fixed-Entry-Accepting-Values

--- src/Controls/src/Core/InputView/InputView.Platform.cs
+++ src/Controls/src/Core/InputView/InputView.Platform.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Microsoft.Maui.Controls
 {
@@ -12,6 +13,17 @@ namespace Microsoft.Maui.Controls
 				handler
 					?.GetService<HideSoftInputOnTappedChangedManager>()
 					?.UpdateFocusForView(iv);
+			}
+		}
+
+		internal static void MapIsVisible(IViewHandler handler, IView view)
+		{
+			if (view is not InputView inputView || handler?.PlatformView == null)
+				return;
+
+			if (!inputView.IsVisible && inputView.IsSoftInputShowing())
+			{
+				inputView.HideSoftInputAsync(CancellationToken.None);
 			}
 		}
 #endif
