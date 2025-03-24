79000cd0a1a2c115fe76ab2404d1b73063df37b7
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-03-24 18:03:17 +0530

    .

--- src/Controls/tests/TestCases.HostApp/Issues/Issue28431.cs
+++ src/Controls/tests/TestCases.HostApp/Issues/Issue28431.cs
@@ -1,15 +1,14 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
-namespace Maui.Controls.Sample.Issues
+namespace Maui.Controls.Sample.Issues;
+public class Issue28431 : ContentPage
 {
-	public class Issue28431 : ContentPage
-	{
-		ObservableCollection<Issue28431ViewItem> Items;
+	ObservableCollection<Issue28431ViewItem> Items;
 
-		public Issue28431()
-		{
-			Items = new ObservableCollection<Issue28431ViewItem>
+	public Issue28431()
+	{
+		Items = new ObservableCollection<Issue28431ViewItem>
 			{
 				new Issue28431ViewItem { Name = "UltraWidget", Margin = new Thickness(20) },
 				new Issue28431ViewItem { Name = "ProGadget", Margin = new Thickness(20) },
@@ -17,94 +16,93 @@ namespace Maui.Controls.Sample.Issues
 				new Issue28431ViewItem { Name = "EliteComponent", Margin = new Thickness(20) },
 				new Issue28431ViewItem { Name = "PrimeUtility", Margin = new Thickness(20) }
 			};
-			Content = CreateLayout();
-		}
+		Content = CreateLayout();
+	}
 
-		Grid CreateLayout()
+	Grid CreateLayout()
+	{
+		Grid mainGrid = new Grid
 		{
-			Grid mainGrid = new Grid
-			{
-				AutomationId = "MainGrid",
-				RowDefinitions =
+			AutomationId = "MainGrid",
+			RowDefinitions =
 			{
 				new RowDefinition { Height = GridLength.Auto },
 				new RowDefinition { Height = GridLength.Star }
 			}
-			};
+		};
 
-			Button changeMarginButton = new Button
-			{
-				Text = "Change Margin",
-				AutomationId = "ChangeMargin"
-			};
-			changeMarginButton.Clicked += OnChangeMarginClicked;
-
-			CollectionView collectionView = new CollectionView
-			{
-				AutomationId = "CollectionView",
-				ItemsSource = Items,
-				ItemTemplate = CreateItemTemplate()
-			};
-
-			mainGrid.Add(changeMarginButton, 0, 0);
-			mainGrid.Add(collectionView, 0, 1);
-
-			return mainGrid;
-		}
-
-		DataTemplate CreateItemTemplate()
+		Button changeMarginButton = new Button
 		{
-			return new DataTemplate(() =>
-			{
-				Label label = new Label
-				{
-					FontSize = 20,
-					HorizontalOptions = LayoutOptions.Start,
-					VerticalOptions = LayoutOptions.Center
-				};
+			Text = "Change Margin",
+			AutomationId = "ChangeMargin"
+		};
+		changeMarginButton.Clicked += OnChangeMarginClicked;
 
-				label.SetBinding(Label.TextProperty, nameof(Issue28431ViewItem.Name));
-				label.SetBinding(Label.MarginProperty, nameof(Issue28431ViewItem.Margin));
-
-				return new Grid
-				{
-					Children = { label }
-				};
-			});
-		}
-
-		void OnChangeMarginClicked(object sender, EventArgs e)
+		CollectionView collectionView = new CollectionView
 		{
-			foreach (var item in Items)
+			AutomationId = "CollectionView",
+			ItemsSource = Items,
+			ItemTemplate = CreateItemTemplate()
+		};
+
+		mainGrid.Add(changeMarginButton, 0, 0);
+		mainGrid.Add(collectionView, 0, 1);
+
+		return mainGrid;
+	}
+
+	DataTemplate CreateItemTemplate()
+	{
+		return new DataTemplate(() =>
+		{
+			Label label = new Label
 			{
-				item.Margin = new Thickness(60);
+				FontSize = 20,
+				HorizontalOptions = LayoutOptions.Start,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			label.SetBinding(Label.TextProperty, nameof(Issue28431ViewItem.Name));
+			label.SetBinding(Label.MarginProperty, nameof(Issue28431ViewItem.Margin));
+
+			return new Grid
+			{
+				Children = { label }
+			};
+		});
+	}
+
+	void OnChangeMarginClicked(object sender, EventArgs e)
+	{
+		foreach (var item in Items)
+		{
+			item.Margin = new Thickness(60);
+		}
+	}
+}
+
+class Issue28431ViewItem : INotifyPropertyChanged
+{
+	public string Name { get; set; }
+
+	Thickness _margin;
+	public Thickness Margin
+	{
+		get => _margin;
+		set
+		{
+			if (_margin != value)
+			{
+				_margin = value;
+				OnPropertyChanged(nameof(Margin));
 			}
 		}
 	}
 
-	class Issue28431ViewItem : INotifyPropertyChanged
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected void OnPropertyChanged(string propertyName)
 	{
-		public string Name { get; set; }
-
-		Thickness _margin;
-		public Thickness Margin
-		{
-			get => _margin;
-			set
-			{
-				if (_margin != value)
-				{
-					_margin = value;
-					OnPropertyChanged(nameof(Margin));
-				}
-			}
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		protected void OnPropertyChanged(string propertyName)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
