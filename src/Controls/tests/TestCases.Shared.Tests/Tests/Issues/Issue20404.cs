3d789d909dcf46f8b0e0a294349182331db376e5
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-06-20 17:20:14 +0530

    Modified-TestFile

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20404.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20404.cs
@@ -9,16 +9,15 @@ public class Issue20404 : _IssuesUITest
 	public Issue20404(TestDevice testDevice) : base(testDevice) { }
 
 	public override string Issue => "Dynamic Grid Row/Column changes don't trigger layout update on Windows until window resize";
-	const string StatusLabel = "StatusLabel";
 
 	[Test]
 	[Category(UITestCategories.Layout)]
 	public void DynamicGridRowColumnChangeShouldInvalidate()
 	{
-		App.WaitForElement(StatusLabel);
+		App.WaitForElement("ToggleRowButton");
 		App.Tap("ToggleRowButton");
 		VerifyScreenshot("AfterGridRowToggled");
-		App.WaitForElement(StatusLabel);
+		App.WaitForElement("ToggleColumnButton");
 		App.Tap("ToggleColumnButton");
 		VerifyScreenshot("AfterGridColumnToggled");
 	}
