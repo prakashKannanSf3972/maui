55b35357288c7d13958c36525fac6d6b32f217c3
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-06-24 13:24:54 +0530

    Fixed-Background-Rendering-Issue

--- src/Core/src/Platform/Windows/LayoutPanel.cs
+++ src/Core/src/Platform/Windows/LayoutPanel.cs
@@ -9,7 +9,6 @@ namespace Microsoft.Maui.Platform
 {
 	public class LayoutPanel : MauiPanel
 	{
-		Canvas? _backgroundLayer;
 		public bool ClipsToBounds { get; set; }
 
 		// TODO: Possibly reconcile this code with ViewHandlerExtensions.LayoutVirtualView
@@ -29,78 +28,24 @@ namespace Microsoft.Maui.Platform
 
 		public void UpdateInputTransparent(bool inputTransparent, Brush? background)
 		{
-			if (inputTransparent)
-			{
-				MakeInputTransparent(background);
-			}
-			else
-			{
-				MakeInputVisible(background);
-			}
+			// Set hit test visibility based on input transparency
+			IsHitTestVisible = !inputTransparent;
+
+			SetBackground(background);
 		}
 
-		void MakeInputTransparent(Brush? background)
+		void SetBackground(Brush? background)
 		{
-			Background = null;
-
-			if (background == null)
-			{
-				// If the background is null, we don't need the background layer
-				RemoveBackgroundLayer();
-			}
-			else
-			{
-				// Add the background layer to handle the background brush
-				AddBackgroundLayer();
-				_backgroundLayer!.Background = background;
-			}
-		}
-
-		void MakeInputVisible(Brush? background)
-		{
-			// If we aren't input transparent, we don't need the background layer hack 
-			RemoveBackgroundLayer();
-
-			if (background == null)
+			if (background is null)
 			{
 				// We can't have a null background, because that would allow input through
 				// So we'll make the background color transparent (visually the same as null, but consumes input)
-				background = new WSolidColorBrush(UI.Colors.Transparent);
+				Background = new WSolidColorBrush(UI.Colors.Transparent);
 			}
-
-			Background = background;
-		}
-
-		void AddBackgroundLayer()
-		{
-			// In WinUI, once a control has hit testing disabled, all of its child controls
-			// also have hit testing disabled. The exception is a Panel with its 
-			// Background Brush set to `null`; the Panel will be invisible to hit testing, but its
-			// children will work just fine. 
-
-			// In order to handle the situation where we need the layout to be invisible to hit testing,
-			// the child controls to be visible to hit testing, *and* we need to support non-null
-			// background brushes, we insert another empty Panel which is invisible to hit testing; that
-			// Panel will be our Background brush
-
-			if (_backgroundLayer != null)
+			else
 			{
-				return;
+				Background = background;
 			}
-
-			_backgroundLayer = new Canvas { IsHitTestVisible = false };
-			CachedChildren.Insert(0, _backgroundLayer);
-		}
-
-		void RemoveBackgroundLayer()
-		{
-			if (_backgroundLayer == null)
-			{
-				return;
-			}
-
-			CachedChildren.Remove(_backgroundLayer);
-			_backgroundLayer = null;
 		}
 	}
 }
